be797b23dd3fe8f5f489feb7c4ce30ea0bc16997
Davyd-proger <58080797+Davyd-proger@users.noreply.github.com>
2020-12-18 14:51:35 +0200

    Add files via upload

---
+++ src/test/java/simplecalc/exam1/examTest.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simplecalc.exam1;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author ц
+ */
+public class examTest {
+    
+    public examTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of testArray method, of class exam.
+     */
+    @Test
+    public void testTestArray() {
+        System.out.println("testArray");
+        exam.testArray();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
